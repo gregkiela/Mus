7b7d069b6a1a549eef7a12dc4b1a3b932b824000
Guigui laverge <glavergne001@iutbayonne.univ-pau.fr>
2022-02-09 17:35:38 +0100

    Modification de la classe equipe et de la classe opposants afin d'ajouter un principe d'equipe

--- src/main/java/com/montaury/mus/jeu/Opposants.java
+++ src/main/java/com/montaury/mus/jeu/Opposants.java
@@ -1,6 +1,7 @@
 package com.montaury.mus.jeu;
 
 import com.montaury.mus.jeu.joueur.Joueur;
+import com.montaury.mus.jeu.joueur.Equipe;
 import java.util.Iterator;
 import java.util.List;
 
@@ -8,16 +9,39 @@ public class Opposants {
   private Joueur joueurEsku;
   private Joueur joueurZaku;
 
+/*
+  private Equipe equipeEsku;
+  private Equipe equipeZaku;
+ */
+
   public Opposants(Joueur joueurEsku, Joueur joueurZaku) {
     this.joueurEsku = joueurEsku;
     this.joueurZaku = joueurZaku;
   }
 
+  /*
+  public Opposants(Equipe equipeEsku, Equipe equipeZaku)
+  {
+    this.equipeEsku=equipeEsku;
+    this.equipeZaku=equipeZaku;
+  }
+   */
   public void tourner() {
     var tmp = joueurEsku;
     joueurEsku = joueurZaku;
     joueurZaku = tmp;
   }
+
+  /*
+  public void tournerEquipe()
+  {
+    var tmp = equipeEsku;
+    equipeEsku=equipeZaku;
+    equipeZaku= tmp;
+
+    equipeZaku.tourner();
+  }
+   */
 
   public Joueur joueurEsku() {
     return joueurEsku;
@@ -30,4 +54,26 @@ public class Opposants {
   public List<Joueur> dansLOrdre() {
     return List.of(joueurEsku, joueurZaku);
   }
+
+  /*
+  public Joueur joueurEsku()
+  {
+    return equipeEsku.joueurEsku();
+  }
+   */
+
+  /*
+  public Joueur joueurZaku()
+  {
+    return equipeZaku.joueurZaku();
+  }
+   */
+
+
+  /*
+  public List<Joueur> dansLOrdreEquipe()
+  {
+    return Liste.of(equipeEsku.joueurEsku(),equipeZaku.joueurEsku(),equipeEsku.joueurZaku(),equipeZaku.joueurZaku());
+  }
+   */
 }
--- src/main/java/com/montaury/mus/jeu/joueur/Equipe.java
+++ src/main/java/com/montaury/mus/jeu/joueur/Equipe.java
@@ -10,38 +10,25 @@ public class Equipe {
 
     /*LES ATTRIBUTS*/
     private String nomEquipe;//nom de l'equipe en string
-    private int identifiant;//identifiant de l'equipe etant egal a 1 ou 2
-    private Joueur joueur1;//joueur numero 1 de l'equipe
-    private Joueur joueur2;//joueur numero 2 de l'equipe
+    private Joueur joueurEsku;//joueur numero 1 de l'equipe
+    private Joueur joueurZaku;//joueur numero 2 de l'equipe
 
     /*CONSTRUCTEUR*/
 
-    //constructeur demandant le joueur 1, le joueur 2, son nom et son identifiant qui vaut 1 ou 2
-    public Equipe(Joueur j1, Joueur j2, String nom,int id)
+    //constructeur demandant le joueur 1, le joueur 2 et son nom
+    public Equipe(Joueur j1, Joueur j2, String nom)
     {
-        joueur1=j1;
-        joueur2=j2;
+        joueurEsku=j1;
+        joueurZaku=j2;
         nomEquipe=nom;
-        identifiant=id;
     }
 
     /*METHODES*/
 
-    //retourne le joueur 1 ou 2 celon le numero passé en paramétre
-    //affiche une erreur si le numero n'est ni 1 ni 2
-    public Joueur getJoueur(int numero) {
-        if(numero==1)
-        {
-            return joueur1;
-        }
-        else if(numero==2)
-        {
-            return joueur2;
-        }
-        else
-        {
-            System.out.print("Erreur, joueur introuvable");
-            return new Joueur("vide", new InterfaceJoueurHumain());
-        }
+    public void tourner()
+    {
+        var tmp= joueurEsku;
+        joueurEsku=joueurZaku;
+        joueurZaku=tmp;
     }
 }
